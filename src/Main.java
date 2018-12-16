65ba6258ff309808a3208b85a7c724514a5fe971
Oguzhant <oguzhanturanunlu@gmail.com>
2018-12-16 19:25:35 +0300

    Samet Test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // 12-16-18 07.16 PM GM +3
+        // 12-16-18 07.16 PM GMT +3
         System.out.println("Final control for file updates");
     }
 }
