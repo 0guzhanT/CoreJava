b8babf4c896b5715bc1cf68517fc67d5e127ef75
Oguzhant <oguzhanturanunlu@gmail.com>
2018-12-16 19:02:55 +0300

    .gitignore file has be

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("File contents change test");
+        System.out.println("Lets hech if the file is updated or not");
     }
 }
