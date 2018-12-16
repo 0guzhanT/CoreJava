3d70654b39c799d7fa83b30d16ea69abf2c9b0c4
Oguzhant <oguzhanturanunlu@gmail.com>
2018-12-16 19:21:17 +0300

    Final control for the file updates

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // 12-16-18 07.16 PM
+        // 12-16-18 07.16 PM GM +3
         System.out.println("Final control for file updates");
     }
 }
