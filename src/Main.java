475ff98f78012931604901d5c6fd0632459d102d
Oguzhant <oguzhanturanunlu@gmail.com>
2018-12-16 19:12:11 +0300

    Final control for the file updates

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("File contents change test");
-        System.out.println("Lets hech if the file is updated or not");
+        // 12-16-18
+        System.out.println("Final control for file updates");
     }
 }
